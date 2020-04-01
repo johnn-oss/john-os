82d56768d4599f3806b923130a9f6d818c94a294
john-oss <johnalphonsus3@gmail.com>
2020-04-01 18:12:15 +0300

    Variables

---
+++ Variables.kt
@@ -0,0 +1,15 @@
+
+fun main(args:Array<String>){
+
+    var name="John"
+    var age=28
+    var dep:String?
+    dep="Software engineering"
+    println("name: " + name)
+    println("age: "+ age)
+    println("department: " + dep)
+    var pi:Float=3.4f
+    println(pi)
+    var p:Double=3.5
+    println(p)
+}
